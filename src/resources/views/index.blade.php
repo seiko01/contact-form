071a65854d8107f414cfa0882e72a6711a498746
seiko <seiko.iwao.0715@gmail.com>
2024-11-22 06:24:30 +0900

    バリデーション設定

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -24,7 +24,8 @@
       <div class="contact-form__heading">
         <h2>お問い合わせ</h2>
       </div>
-      <form class="form">
+      <form class="form" action="/contacts/confirm" method="post">
+        @csrf
         <div class="form__group">
           <div class="form__group-title">
             <span class="form__label--item">お名前</span>
@@ -32,11 +33,13 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="text" name="name" placeholder="テスト太郎" />
+            <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
-            </div>
+                  @error('name')
+                  {{ $message }}
+                  @enderror
+          </div>
           </div>
         </div>
         <div class="form__group">
@@ -46,11 +49,13 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="email" name="email" placeholder="test@example.com" />
+            <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
-            </div>
+                  @error('email')
+                  {{ $message }}
+                  @enderror
+          </div>
           </div>
         </div>
         <div class="form__group">
@@ -60,10 +65,12 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="tel" name="tel" placeholder="09012345678" />
+            <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+                  @error('tel')
+                  {{ $message }}
+                  @enderror
             </div>
           </div>
         </div>
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index']);
+Route::POST('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
