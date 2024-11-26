b5519ebba88380de8a22f550e5dff31799540d39
seiko <seiko.iwao.0715@gmail.com>
2024-11-26 13:06:55 +0900

    TODOアプリ中級

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,37 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Todo;
-use Illuminate\Http\Request;
 use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
+use App\Models\Category;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-public function index()
-{
-$todos = Todo::all();
+  public function index()
+  {
+    $todos = Todo::with('category')->get();
+    $categories = Category::all();
 
-return view('index', compact('todos'));
-}
-    public function store(TodoRequest $request)
-        {
-        $todo = $request->only(['content']);
-        Todo::create($todo);
+    return view('index', compact('todos', 'categories'));
+  }
 
-        return redirect('/')->with('message', 'Todoを作成しました');
-    }
+  public function store(TodoRequest $request)
+  {
+    $todo = $request->only(['category_id', 'content']);
+    Todo::create($todo);
 
-public function update(TodoRequest $request)
-{
+    return redirect('/')->with('message', 'Todoを作成しました');
+  }
+
+  public function update(TodoRequest $request)
+  {
     $todo = $request->only(['content']);
     Todo::find($request->id)->update($todo);
 
     return redirect('/')->with('message', 'Todoを更新しました');
+  }
+
+  public function destroy(Request $request)
+  {
+    Todo::find($request->id)->delete();
+
+    return redirect('/')->with('message', 'Todoを削除しました');
+  }
+  public function search(Request $request)
+{
+  $todos = Todo::with('category')->CategorySearch($request->category_id)->KeywordSearch($request->keyword)->get();
+  $categories = Category::all();
+
+  return view('index', compact('todos', 'categories'));
 }
 
-public function destroy(Request $request)
-{
-    Todo::find($request->id)->delete();
-    return redirect('/')->with('message', 'Todoを削除しました');
-}
 }
